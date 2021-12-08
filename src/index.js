7eb08a97d7e61219f46ae238976a573fcf78ba6e
Oleksandr <81925784+NOSOV-ksa@users.noreply.github.com>
2021-12-08 23:01:21 +0200

    hbs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import tempateItem from './templates/templateItem.hbs';
+import fetch from './fetchCountries';
+import { debounce } from 'debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const refs = {
+    inputSearch: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info')
+};
+
+refs.inputSearch.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput (event) {
+    event.preventDefault();
+    const nameCountry = event.target.value.trim();
+    fetch.fetchCountries(nameCountry)
+        .then((country) => {
+            console.log(country)
+            if (nameCountry === '') {
+                refs.countryInfo.innerHTML = '';
+                refs.countryList.innerHTML = '';
+                return;
+            }
+            if (country.length === 1) {
+                const markup = tempateItem(country);
+                refs.countryInfo.innerHTML = markup;
+            }
+        })
+        .catch();
+}
